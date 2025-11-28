4a1ff339966a2953c1786f635716f541e6b8cfbc
mostafa elabsawy <mostfaelabsawy@gmail.com>
2025-11-28 10:45:13 +0200

    database link updated

--- Back End/DB/connection.js
+++ Back End/DB/connection.js
@@ -2,7 +2,7 @@ import moongose from 'mongoose';
 export const connectDB = async () =>
 { 
     return moongose.connect(
-      "mongodb+srv://mostafaelabsawy:NbgIIBUeJKqXvQH7@cluster0.9vk1zzw.mongodb.net/nti_gig"
+      "DataBase URL here"
     ).then(() => {
       console.log("DataBase connected successfully");
     }).catch((err) => {
