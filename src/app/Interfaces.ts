72f957541cc3ed49ce43216b22b94d1985e69b49
Mostafa-Elabsawy <mostfaelabsawy@gmail.com>
2025-11-28 08:58:20 +0200

    front to be continued

---
+++ src/app/Interfaces.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  _id: string;
+  __v: number;
+  name: string;
+  price: number;
+  category: string;
+}
---
+++ src/app/services/products-api.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from '../Interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsAPi {
+  http = inject(HttpClient);
+  private baseUrl = 'https://basic-crud-operations-production.up.railway.app/products/';
+  getProducts() {
+    return this.http.get<{ message: string; Products: Product[] }>(this.baseUrl);
+  }
+  updateProduct(id: string, data: Partial<Product>) {
+    data._id = id;
+    const requestBody={id, ...data};
+    return this.http.put(this.baseUrl, requestBody);
+  }
+  deleteProduct(id: string) {
+    return this.http.delete(this.baseUrl+id); 
+  }
+}
